26b9c93244b9a84e0ea76d4561433f63342aa037
krainov <krainov@bpcbt.com>
2014-12-22 17:37:23 +0300

    add

---
+++ src/main/java/ru/yesdo/model/SearchProductCriteria.java
@@ -0,0 +1,14 @@
+package ru.yesdo.model;
+
+/**
+ * User: Krainov
+ * Date: 22.12.2014
+ * Time: 18:35
+ * Класс для поиска
+ */
+public class SearchProductCriteria {
+
+    private Double startPrice;
+    private Double endPrice;
+    private Location location;
+}
